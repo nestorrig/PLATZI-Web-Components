81e5dce2d6ddc1ac095164ea1aa312255f5e45e9
nestorrig <neriga10@hotmail.com>
2023-11-09 22:20:04 -0600

    Clase 7: Custom elements; usar this para template

--- CustomElements/my-element.js
+++ CustomElements/my-element.js
@@ -18,11 +18,24 @@ class myElement extends HTMLElement {
         super()
         console.log("Hola mundo");
         this.p = document.createElement('p')
+        this.template = document.createElement('div')
     }
     connectedCallback() {
         this.p.textContent = "Hola mundo"
+        this.template.innerHTML = `
+            <style>
+                .text {
+                    color: red;
+                }
+                p {
+                    color: blue;
+                }
+            </style>
+            <p class="text">Hola Mundo 2</p>
+            <p>Texto ejemplo para la clase</p>
+        `;
         this.appendChild(this.p)
-        this.appendChild(template)
+        this.appendChild(this.template)
     }
 }
 
